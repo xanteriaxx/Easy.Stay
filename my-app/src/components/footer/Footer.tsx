daefd3d740281c7d4047fb21874eae0bed0804b0
xanteriaxx <jfjdssioosfdkjsfig@gmail.com>
2026-04-08 01:39:03 -0700

    feat: add styled footer and refine offers layout

--- my-app/src/components/footer/Footer.tsx
+++ my-app/src/components/footer/Footer.tsx
@@ -4,6 +4,7 @@ import instagramIconUrl from '../../assets/footer/Instagram_black.svg'
 import phoneIconUrl from '../../assets/footer/Phone_black.svg'
 import telegramIconUrl from '../../assets/footer/Telegram_black.svg'
 import youtubeIconUrl from '../../assets/footer/Youtube_black.svg'
+import logoUrl from '../../assets/header/Logo.svg'
 
 const footerColumns = [
   {
@@ -32,48 +33,49 @@ export const Footer = () => {
   return (
     <footer className="site-footer">
       <div className="site-footer__inner">
-        <div className="site-footer__top">
-          {footerColumns.map((column) => (
-            <div key={column.title} className="site-footer__column">
-              <h3 className="site-footer__title">{column.title}</h3>
+        <div className="site-footer__content">
+          <div className="site-footer__top">
+            {footerColumns.map((column) => (
+              <div key={column.title} className="site-footer__column">
+                <h3 className="site-footer__title">{column.title}</h3>
 
-              <div className="site-footer__links">
-                {column.links.map((link) => (
-                  <a key={link} className="site-footer__link" href="#">
-                    {link}
-                  </a>
-                ))}
+                <div className="site-footer__links">
+                  {column.links.map((link) => (
+                    <a key={link} className="site-footer__link" href="#">
+                      {link}
+                    </a>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
-
-          <div className="site-footer__subscribe">
-            <h3 className="site-footer__title">გამოიწერეთ</h3>
-            <p className="site-footer__subscribe-copy">ელ. ფოსტა</p>
-
-            <form className="site-footer__form">
-              <input className="site-footer__input" type="email" aria-label="Email" />
-              <button className="site-footer__submit" type="submit" aria-label="Subscribe">
-                <img className="site-footer__submit-icon" src={telegramIconUrl} alt="" />
-              </button>
-            </form>
-          </div>
-        </div>
-
-        <div className="site-footer__divider" />
-
-        <div className="site-footer__bottom">
-          <a className="site-footer__logo" href="#/">
-            <span className="site-footer__logo-easy">Easy.</span>
-            <span className="site-footer__logo-stay">Stay</span>
-          </a>
-
-          <div className="site-footer__socials">
-            {socialLinks.map((link) => (
-              <a key={link.label} className="site-footer__social" href={link.href} aria-label={link.label}>
-                <img className="site-footer__social-icon" src={link.icon} alt="" />
-              </a>
             ))}
+
+            <div className="site-footer__subscribe">
+              <h3 className="site-footer__title">გამოიწერეთ</h3>
+              <p className="site-footer__subscribe-copy">ელ. ფოსტა</p>
+
+              <form className="site-footer__form">
+                <input className="site-footer__input" type="email" aria-label="Email" />
+                <button className="site-footer__submit" type="submit" aria-label="Subscribe">
+                  <img className="site-footer__submit-icon" src={telegramIconUrl} alt="" />
+                </button>
+              </form>
+            </div>
+          </div>
+
+          <div className="site-footer__divider" />
+
+          <div className="site-footer__bottom">
+            <a className="site-footer__logo" href="#/">
+              <img className="site-footer__logo-image" src={logoUrl} alt="Easy Stay" />
+            </a>
+
+            <div className="site-footer__socials">
+              {socialLinks.map((link) => (
+                <a key={link.label} className="site-footer__social" href={link.href} aria-label={link.label}>
+                  <img className="site-footer__social-icon" src={link.icon} alt="" />
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
